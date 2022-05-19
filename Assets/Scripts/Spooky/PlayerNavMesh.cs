7aa2aafd97e8c82033f0086a9a24e6feedb04bce
Fabricio-F16 <vini.161092@gmail.com>
2022-05-18 23:45:26 -0300

    Otimização Spook

--- Assets/Scripts/Spooky/PlayerNavMesh.cs
+++ Assets/Scripts/Spooky/PlayerNavMesh.cs
@@ -9,9 +9,11 @@ public class PlayerNavMesh : MonoBehaviour
     public Transform destination;
 
     private NavMeshAgent _navMeshAgent;
+    private Animator animator;
 
     private void Awake()
     {
+        animator = GetComponent<Animator>();
         _navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
@@ -27,11 +29,19 @@ public class PlayerNavMesh : MonoBehaviour
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+            animator.SetBool("IsMoving", true);
 
             if (Physics.Raycast(ray, out hit))
             {
                 _navMeshAgent.SetDestination(hit.point);
+                
             }
+            
+        }
+    else
+        {
+            animator.SetBool("IsMoving",false);
+            animator.SetBool("Idle",true);
         }
     }
 }
